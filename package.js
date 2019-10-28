8ef7d9b12dd7c96ead54f1e41ce4d9036128a7ca
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-28 01:35:45 -0400

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Exposing internal Meteor APIs to hack Meteor easily",
-  version: "2.1.2",
+  version: "2.1.3",
   git: "https://github.com/lamhieu-vk/meteorx.git",
   name: "lamhieu:meteorx",
 });

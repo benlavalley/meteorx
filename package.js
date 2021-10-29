fc26faa00f4c9662f26f0d30efbc4f0721197f62
Ben Lavalley <ben@nibbl.com>
2021-10-28 23:31:59 -0400

    compatibility update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Exposing internal Meteor APIs to hack Meteor easily",
-  version: "2.1.3",
+  version: "2.1.4",
   git: "https://github.com/lamhieu-vk/meteorx.git",
   name: "lamhieu:meteorx",
 });
@@ -13,11 +13,11 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   configurePackage(api);
   api.use(["tinytest"], "server");
-  api.add_files(["test/server.js"], "server");
+  api.addFiles(["test/server.js"], "server");
 });
 
 function configurePackage(api) {
-  api.versionsFrom("METEOR@1.8.1");
+  api.versionsFrom("METEOR@2.5");
   api.use(["random", "mongo"], "server");
-  api.add_files(["src/livedata.js", "src/mongo-livedata.js", "src/server.js"], "server");
+  api.addFiles(["src/livedata.js", "src/mongo-livedata.js", "src/server.js"], "server");
 }
